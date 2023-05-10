0c8be73f325597de10cb132209e73531fa1539fd
Reshetniak Maksim <resetnak32@gmail.com>
2023-05-10 19:32:19 +0400

    Added solution task from leetcode, Promise time limit Exceeded

---
+++ PromiseTimeLimit/time_limit_setInterval.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/promise-time-limit/description/
+
+var timeLimit = function (fn, t) {
+    return async function (...args) {
+        const originalFnPromise = fn(...args);
+
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject('Time Limit Exceeded')
+            }, t);
+        })
+
+        return Promise.race([originalFnPromise, timeoutPromise]);
+    }
+};
