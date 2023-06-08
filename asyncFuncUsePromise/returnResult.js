7b7c3080d6a9c48fede50aa079617749d2fb44a7
Reshetniak Maksim <resetnak32@gmail.com>
2023-06-08 19:12:54 +0400

    Added task sleep(Promise)

---
+++ asyncFuncUsePromise/returnResult.js
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/sleep/
+
+async function sleep(millis) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, millis);
+    })
+}
